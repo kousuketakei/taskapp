259f1c149600dbca0e58ff488324dac1b336a9b7
Ryunosuke Muramatsu <ryunosuke@altive.jp>
2019-10-22 12:10:24 +0900

    Errorを解消、Task.swiftの追加

--- taskapp/inputViewController.swift
+++ taskapp/inputViewController.swift
@@ -24,19 +24,18 @@ class inputViewController: UIViewController {
         super.viewDidLoad()
         
         //背景をタップしたらdismissKeyboardを呼ぶように設定する
-        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
         
         titleTextField.text = task.title
-        contentsTextView.text = task.contents
+        contentsTextField.text = task.contents
         datePicker.date = task.date
-        
-        // Do any additional setup after loading the view.
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         try! realm.write {
             self.task.title = self.titleTextField.text!
-            self.task.contents = self.contentsTextView.text
+            self.task.contents = self.contentsTextField.text
             self.task.date = self.datePicker.date
             self.realm.add(self.task, update: true)
             
@@ -45,6 +44,7 @@ class inputViewController: UIViewController {
         
         super.viewWillDisappear(animated)
     }
+    
     //タスクのローカル通知を登録する
     func setNotification(task: Task) {
         let content = UNMutableNotificationContent()
@@ -60,52 +60,42 @@ class inputViewController: UIViewController {
             content.body = task.contents
         }
         content.sound = UNNotificationSound.default
-    
-    
-    //ローカル通知が発動するtrigger(日付マッチ)を作成
-    let calendar = Calendar.current
-    let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
-    let trigger = UNCalendarNotificationTrigger.init(dateMatching:
-        dateComponents, repeats: false)
-    
-    //identifier, content, triggerからローカル通知を作成（identifierが同じだとローカル通知を上書き保存)
-    let request = UNNotificationRequest.init(identifier: String(task.id), content: content, trigger: trigger)
-    
-    //ローカル通知を登録
-    let center = UNUserNotificationCenter.current()
-    center.add(request) { (error)  in
-    print(error ?? "ローカル通知登録 OK") //errorがnilならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
-    
+        
+        
+        //ローカル通知が発動するtrigger(日付マッチ)を作成
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching:
+            dateComponents, repeats: false)
+        
+        //identifier, content, triggerからローカル通知を作成（identifierが同じだとローカル通知を上書き保存)
+        let request = UNNotificationRequest.init(identifier: String(task.id), content: content, trigger: trigger)
+        
+        //ローカル通知を登録
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error)  in
+            print(error ?? "ローカル通知登録 OK") //errorがnilならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
+            
+        }
+        
+        //未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests{ (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------")
+                print(request)
+                print("/---------")
+            }
+            
+        }
+        
+        
+        
+        
     }
     
-    //未通知のローカル通知一覧をログ出力
-    center.getPendingNotificationRequests{ (requests: [UNNotificationRequest]) in
-    for request in requests {
-    print("/---------")
-    print(request)
-    print("/---------")
-    }
-    
-    }
-    
-    
-    
-    
-        func dismissKeyboard() {
+    @objc func dismissKeyboard() {
         //キーボードを閉じる
         view.endEditing(true)
     }
-    
-    }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
